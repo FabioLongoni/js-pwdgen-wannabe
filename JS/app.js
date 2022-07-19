3c00792348a7a8179f1900dca71a8dc2c2cc81e9
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-19 15:25:09 +0200

    add-console.log

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,12 @@
 
 let yourName = prompt('whats your name?')
+console.log(yourName)
 
 let yourLastName = prompt('whats your lastname?')
+console.log(yourLastName)
 
 let yourFavColor = prompt('whats your favourite color?')
+console.log(yourFavColor)
 
 const passwordGenerator = 'yourName' + 'yourLastname' + 'yourFavColor' + 21
 
